9a1bb14ab7622430c9df78b93ad158152eaac4ec
erqiang.zhang <erqiang.zhang@baichanghui.com>
2017-01-17 00:07:45 +0800

    add koala-cache

---
+++ src/test/java/com/kyrincloud/koala_cache/AppTest.java
@@ -0,0 +1,21 @@
+package com.kyrincloud.koala_cache;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.store.MemoryStoreEvictionPolicy;
+
+public class AppTest{
+	
+	public static void main(String[] args) {
+		CacheManager manager = CacheManager.getInstance();
+		Cache cache = new Cache("cache-1", 1024 * 10, MemoryStoreEvictionPolicy.FIFO, 
+				true, "/Users/zhangerqiang/Desktop/cache", true, 60 * 60 * 1000, 60, true, 60, null);
+		manager.addCache(cache);
+		Cache c = manager.getCache("cache-1");
+		Element element = new Element("name", "kyrin");
+		c.put(element);
+		c.flush();
+	}
+	
+}
