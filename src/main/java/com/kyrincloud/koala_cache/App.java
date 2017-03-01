47095718f27e33eaed472e8fe22e8ef5c6cd99f0
erqiang.zhang <erqiang.zhang@baichanghui.com>
2017-03-01 23:27:54 +0800

    update

--- src/main/java/com/kyrincloud/koala_cache/App.java
+++ src/main/java/com/kyrincloud/koala_cache/App.java
@@ -1,13 +1,30 @@
 package com.kyrincloud.koala_cache;
 
 /**
- * Hello world!
+ * 单机存储引擎验证
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+//    	Cache cache = new Cache("/tmp/index", "/tmp/data");
+//    	for(int i = 0 ; i< 2;i++){
+//    		String key = i+"";
+//    		for(int j = key.length();j<32;j++){
+//    			key+="0";
+//    		}
+//    		cache.put(key);
+//    	}
+//    	cache.flush();
+    	
+    	IndexCache index = new IndexCache("/tmp/index", "/tmp/data");
+    	for(;;){
+    	System.out.println(System.currentTimeMillis());
+    	System.out.println(index.search("00000000000000000000000000000000"));
+    	System.out.println(System.currentTimeMillis());
+    	}
+
+    	
     }
 }
