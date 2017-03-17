fde870dfdf873f354e1ddbab8990e5a76f715ce4
kyrin <zhangerqiang@meituan.com>
2017-03-17 17:16:39 +0800

    各种比较器实现，DefaultComparator，IPComparator

---
+++ src/main/java/com/kyrincloud/koala_cache/comparator/AbstractComparator.java
@@ -0,0 +1,18 @@
+package com.kyrincloud.koala_cache.comparator;
+
+import com.kyrincloud.koala_cache.Entity;
+import com.kyrincloud.koala_cache.Slice;
+
+/**
+ * 一个比较器是用来拿搜索的key跟每个Entity进行比较的工具，独立出来可以让用户自己定义比较的方式，也许是简单的
+ * key-key类型精确比较，也许是k -（k,v）的范围比较，无论什么你都可以自定义
+ * @author kyrin
+ *
+ * @param <K>
+ * @param <T>
+ */
+public abstract class AbstractComparator{
+
+	public abstract int compare(Slice o1, Entity o2);
+
+}
---
+++ src/main/java/com/kyrincloud/koala_cache/comparator/DefaultComparator.java
@@ -0,0 +1,21 @@
+package com.kyrincloud.koala_cache.comparator;
+
+import com.kyrincloud.koala_cache.Entity;
+import com.kyrincloud.koala_cache.Slice;
+
+/**
+ * default comparator  
+ * 
+ * default k-k类型的精确查找比较
+ * 
+ * @author kyrin
+ *
+ */
+public class DefaultComparator extends AbstractComparator{
+
+	@Override
+	public int compare(Slice o1, Entity o2) {
+		return o1.compareTo(o2.getKey());
+	}
+
+}
